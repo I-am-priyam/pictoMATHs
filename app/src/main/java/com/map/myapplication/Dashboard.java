902d30e8155ddb8118e0581f2980f94f25f6e757
i-am-priyam <priyam.saha.979@gmail.com>
2020-11-28 19:24:09 +0530

    Added Scientific Calculator and removed errors

--- app/src/main/java/com/map/myapplication/Dashboard.java
+++ app/src/main/java/com/map/myapplication/Dashboard.java
@@ -2,10 +2,17 @@ package com.map.myapplication;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -23,6 +30,11 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
 public class Dashboard extends AppCompatActivity {
 
     private static final int IMAGE_REQUEST=1;
@@ -32,6 +44,7 @@ public class Dashboard extends AppCompatActivity {
     Button displayBtn;
     ImageView help,capture,cal;
     TextView tv;
+    private static final int PERMISSION_REQUEST_CODE = 200;
     Animation topAnim;
 
     @Override
@@ -42,6 +55,13 @@ public class Dashboard extends AppCompatActivity {
         topAnim= AnimationUtils.loadAnimation(this,R.anim.top_anim);
         tv=findViewById(R.id.textView);
         tv.setAnimation(topAnim);
+        if (!checkPermission()) {
+
+            requestPermission();
+
+        }
+
+
         cal=findViewById(R.id.cal);
         cal.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -79,6 +99,11 @@ public class Dashboard extends AppCompatActivity {
         capture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                int id = view.getId();
+
+
+
+
                 Intent cameraIntent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
                 if(cameraIntent.resolveActivity(getPackageManager())!=null)
@@ -102,6 +127,69 @@ public class Dashboard extends AppCompatActivity {
 
 
     }
+    private boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE);
+
+        int result1 = ContextCompat.checkSelfPermission(getApplicationContext(), CAMERA);
+        int result2 = ContextCompat.checkSelfPermission(getApplicationContext(), READ_EXTERNAL_STORAGE);
+
+        return result == PackageManager.PERMISSION_GRANTED && result1 == PackageManager.PERMISSION_GRANTED && result1 == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermission() {
+
+        ActivityCompat.requestPermissions(this, new String[]{WRITE_EXTERNAL_STORAGE, CAMERA,READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0) {
+
+                    boolean locationAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean cameraAccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+
+
+
+                    // Snackbar.make(view, "Permission Denied, You cannot access location data and camera.", Snackbar.LENGTH_LONG).show();
+
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (shouldShowRequestPermissionRationale(WRITE_EXTERNAL_STORAGE)) {
+                            showMessageOKCancel("You need to allow access to both the permissions",
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                                requestPermissions(new String[]{WRITE_EXTERNAL_STORAGE, CAMERA,READ_EXTERNAL_STORAGE},
+                                                        PERMISSION_REQUEST_CODE);
+                                            }
+                                        }
+                                    });
+                            return;
+                        }
+                    }
+
+
+                }
+
+
+                break;
+        }
+    }
+
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(Dashboard.this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
+    }
+
+
     private File getImageFile() throws IOException
     {
         String timeStamp=new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
